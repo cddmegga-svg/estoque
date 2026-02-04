4d78f4e483ddd930d6607cfa737914146425dcba
Robson_Forte <robson.rodri@gmail.com>
2026-02-04 09:21:18 -0300

    faxi

--- database/migrations/DEBUG_PERMISSIONS.sql
+++
@@ -1,6 +0,0 @@
--- CHECK CURRENT USER PERMISSIONS
--- Replace 'seu-email@exemplo.com' if known, or just list all to see.
-
-SELECT id, email, role, permissions FROM users;
-
-SELECT * FROM employees;
